bef9a4021ce102994d7c780e47155df65e99d653
Cristobal Moya <CristobalMoyaLorente00@gmail.com>
2024-03-11 22:04:31 +0100

    Terminamos de poner los tweets correctos de la api usando bien sus nombres

--- Proyecto/tweet-model.js
+++ Proyecto/tweet-model.js
@@ -25,13 +25,27 @@
   }];
   */ 
 
-  export function getTweets() {
+// Esta funciones muy IMPORTANTE ya que en el API los nombres no son igual que los de nuestra vista. Para no cambiarlos en la vista creamos esta funcion.
+function parseTweets(data) {
+  return data.map(data => ({
+    handler: data.author,
+    date: new Date().toISOString(),
+    message: data.message,
+    likes: data.likes.length, // Ponemos el lenght por que nosotros esperamos un numero y el API nos devuelve un array de string
+  }))
+}
+
+export function getTweets() {
   const url = 'https://fake-tweets-api-kc.vercel.app/posts';
 
-    return new Promise(function(resolve, reject) {
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {resolve(data)}) // Aqui damos por termianda la promesa y ponemos el resolve para mandarlos al controlador
-        .catch(() => reject('Error al obtener los tweets'))
-    })
-  }
+  return new Promise(function(resolve, reject) {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        const tweets = parseTweets(data)
+        resolve(tweets); // Aqui damos por termianda la promesa y ponemos el resolve para mandarlos al controlador
+      }) 
+      .catch(() => reject('Error al obtener los tweets'))
+  })
+}
+
