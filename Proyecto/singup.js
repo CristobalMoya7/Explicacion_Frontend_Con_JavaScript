7e6984492521c704c924b5c1b91cdf93a2edb311
Cristobal Moya <CristobalMoyaLorente00@gmail.com>
2024-03-14 22:41:44 +0100

    Comenzamos con el singup

---
+++ Proyecto/singup.js
@@ -0,0 +1,12 @@
+
+const singupForm = document.querySelector('#register-form')
+
+singupForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // El preventDefault es para evitar la validacion en el servidor
+
+    // Validaciones del formulario
+
+    // Email con formato correcto
+
+    // Las dos password deben coincidir
+})
